f6b8dbd544404e1b63f5e17e978b10050418dc79
Alex Pena <pena.alex@gmail.com>
2015-03-03 02:12:44 -0800

    changing parameters for travis ci

--- test/plugin/test_in_spectrum.rb
+++ test/plugin/test_in_spectrum.rb
@@ -6,11 +6,9 @@ class SpectrumInputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
-    endpoint spectrumapi.test.com
     user username
     pass password
     interval 60
-    include_raw true
     tag alert.spectrum
   ]
 
@@ -20,11 +18,11 @@ class SpectrumInputTest < Test::Unit::TestCase
 
   def test_configure
     d = create_driver('')
-    assert_equal "spectrumapi.test.com", d.instance.endpoint
+    assert_equal "pleasechangeme.com", d.instance.endpoint
     assert_equal "username", d.instance.user
     assert_equal "password", d.instance.pass
     assert_equal "60".to_i, d.instance.interval
-    assert_equal "true", d.instance.include_raw
+    assert_equal "false", d.instance.include_raw
     assert_equal 'alert.spectrum', d.instance.tag
   end
 end
