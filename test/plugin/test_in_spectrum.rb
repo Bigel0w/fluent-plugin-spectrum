640243a0911c0d97b4ebbe5d69e7e7e1315be027
Alex Pena <pena.alex@gmail.com>
2015-03-31 02:26:34 -0700

    fixing test cases since I removed defaults for user,pass and endpoint

--- test/plugin/test_in_spectrum.rb
+++ test/plugin/test_in_spectrum.rb
@@ -14,9 +14,9 @@ class SpectrumInputTest < Test::Unit::TestCase
 
   def test_configure
     d = create_driver('')
-    assert_equal "pleasechangeme.com", d.instance.endpoint
-    assert_equal "username", d.instance.user
-    assert_equal "password", d.instance.pass
+    #assert_equal "pleasechangeme.com", d.instance.endpoint
+    #assert_equal "username", d.instance.user
+    #assert_equal "password", d.instance.pass
     assert_equal "300".to_i, d.instance.interval
     assert_equal "false", d.instance.include_raw
     assert_equal 'alert.spectrum', d.instance.tag
